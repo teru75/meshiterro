c60676cd8d6a28506114ac51e0e65461ef6d6a8c
teru75 <love.akg.75@gmail.com>
2020-04-08 18:58:20 +0900

    [fix]refile-key

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,11 +16,3 @@
 //= require turbolinks
 //= require_tree .
 
-var title ="javascriptが使えました";
-alert(title);
-
-$(document).ready(function () {
-  $('.jquery').on('click', function(){
-    $(this).css('color','red');
-  });
-});;
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'e6d273110b10a28809e317e13d28ac60bfc18ec041e5b5967aab7678adb842c63433ed829dcaa281ad0ea5d2174fec9f9db2fe6df1a4865c923648a0ded97a29'
